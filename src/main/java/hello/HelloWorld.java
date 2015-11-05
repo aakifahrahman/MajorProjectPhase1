5af42b61812e791025b9e4d192968a1022dfacd1
aakifahrahman <aakifahrahman@gmail.com>
2015-11-05 23:10:16 +0530

    Calculate total score for a review file, calculate all scores for all reviews in a folder named by book name, sum all review scores, display good or bad based on final score

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -7,8 +7,10 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import opennlp.tools.cmdline.postag.POSModelLoader;
 import opennlp.tools.postag.POSModel;
@@ -22,16 +24,22 @@ import opennlp.tools.util.InvalidFormatException;
 
 public class HelloWorld {
 	
-	private static Map<String, Double> dictionary;
+	private static Map<String, Double> dictionary; //final dictionary created from SentiWordNet of the form word#pos --> weighted average score
 	
 	public static void main(String[] args) {
         try {
         	
-        	dictionary = new HashMap<String, Double>();
+        	dictionary = new HashMap<String, Double>(); //initialise an empty hashmap
     	
-    		SentiCode("senti_word_net.txt");
-    		    		
-			SentenceDetect();
+    		SentiCode("senti_word_net.txt"); //populate the dictionary from SentiWordNet
+    		    	 		
+    		Scanner in = new Scanner(System.in); 
+    		String bookName;
+    		
+    	    System.out.println("Enter a string");
+    	    bookName = in.nextLine(); //enter a book name
+    		
+			reviewABook(bookName); //call function to review a Book
 			
 		} catch (InvalidFormatException e) {
 			// TODO Auto-generated catch block
@@ -41,160 +49,152 @@ public class HelloWorld {
 			e.printStackTrace();
 		}
     }
-
+	
 	//Function to split a review in a text file into sentences
 	
-	public static void SentenceDetect() throws InvalidFormatException,
+	public static void reviewABook(String bookName) throws InvalidFormatException,
 	IOException {
+	    
+	    ArrayList<File> reviews = new ArrayList<File>();
+	    listf(bookName, reviews); //return all files ie. reviews in the directory with the same name as the book
 		
-		File f = new File("review1.txt");
-	    FileInputStream fin = new FileInputStream(f);
-	    byte[] buffer = new byte[(int) f.length()];
-	    new DataInputStream(fin).readFully(buffer);
-	    fin.close();
-	    String paragraph = new String(buffer, "UTF-8");
+	    double bookScore = 0.0;
+	    
+	    for(File bookReview : reviews){ //for each review
+	    
+	    	FileInputStream fin = new FileInputStream(bookReview); //open a file stream
+	    	byte[] buffer = new byte[(int) bookReview.length()];
+	    	new DataInputStream(fin).readFully(buffer); //read the file into a buffer string
+	    	fin.close();
+	    	String paragraph = new String(buffer, "UTF-8");
 
-		// always start with a model, a model is learned from training data
-		InputStream is = new FileInputStream("en-sent.bin");
-		SentenceModel model = new SentenceModel(is);
-		SentenceDetectorME sdetector = new SentenceDetectorME(model);
+	    	//start with the opennlp trained model, a model is learned from training data
+	    	InputStream is = new FileInputStream("en-sent.bin");
+	    	SentenceModel model = new SentenceModel(is);
+	    	SentenceDetectorME sdetector = new SentenceDetectorME(model);
 
-		String sentences[] = sdetector.sentDetect(paragraph);
+	    	String sentences[] = sdetector.sentDetect(paragraph); //detect all sentences in the review file
 
-		int noOfSentences = sentences.length;
+	    	int noOfSentences = sentences.length;
+	    	int i=0;
 		
-		int i=0;
+	    	//model to split each sentence into tokens
 		
-		//Splitting each sentence into tokens
+	    	InputStream is2 = new FileInputStream("en-token.bin");
+	    	TokenizerModel model2 = new TokenizerModel(is2);
+	    	Tokenizer tokenizer = new TokenizerME(model2);
 		
-		InputStream is2 = new FileInputStream("en-token.bin");
-		 
-		TokenizerModel model2 = new TokenizerModel(is2);
-	 
-		Tokenizer tokenizer = new TokenizerME(model2);
-		
-		POSModel model3 = new POSModelLoader().load(new File("en-pos-maxent.bin"));
-		
-		POSTaggerME tagger = new POSTaggerME(model3);
+	    	//model to tag each token with pos tags
+	    	
+	    	POSModel model3 = new POSModelLoader().load(new File("en-pos-maxent.bin"));
+	    	POSTaggerME tagger = new POSTaggerME(model3);
 
-		
-		while(i <noOfSentences){
+	    	//initialize score of the review file as 0
+	    	double reviewScore = 0.0;
+	    	
+	    	//loop through all the sentences
+	    	while(i <noOfSentences){
 			
-			String tokens[] = tokenizer.tokenize(sentences[i]);
-		 			
-			String[] tags = tagger.tag(tokens);
+	    		String tokens[] = tokenizer.tokenize(sentences[i]); //tokenize each sentence
+	    		String[] tags = tagger.tag(tokens); //assign pos tag to each token
 		
-			for(int k = 0 ; k < tags.length; k++){
-			if (tags[k].charAt(0) == 'J')
-				tags[k] = "j" ;
-		    else if (tags[k].charAt(0) == 'V')
-		    	tags[k] = "v" ;
-		    else if (tags[k].charAt(0) == 'N')
-		    	tags[k] = "n" ;
-		    else if (tags[k].charAt(0) == 'R')
-		    	tags[k] = "r" ;
-		    else
-		    	tags[k] = " " ;
-			}
+	    		for(int k = 0 ; k < tags.length; k++){ //code to reduce assigned pos into the 4 basic pos present in SentiWordNet 
+	    			if (tags[k].charAt(0) == 'J')
+	    				tags[k] = "j" ;
+	    			else if (tags[k].charAt(0) == 'V')
+	    				tags[k] = "v" ;
+	    			else if (tags[k].charAt(0) == 'N')
+	    				tags[k] = "n" ;
+	    			else if (tags[k].charAt(0) == 'R')
+	    				tags[k] = "r" ;
+	    			else
+	    				tags[k] = " " ;
+	    		}
 			
-			for (int j = 0; j< tokens.length ; j++){
-				if(!tags[j].isEmpty())
-				System.out.println(tokens[j] + " - " + tags[j]+ " - " + extract(tokens[j], tags[j]));
-			}
-		 			
-			i++;
-		}
-		
-		is.close();
+	    		for (int j = 0; j< tokens.length ; j++){
+	    			reviewScore += extract(tokens[j], tags[j]); //call extract function on each token tag pair, sum up the scores returned
+	    		}
+	    				 			
+	    		i++; //iterate to next sentence
+	    	}
+	    	
+	    	bookScore += reviewScore; //sum over all the review scores to get bookscore
+	    	is.close();
+	    }
+	    
+	    if((int)bookScore > 0)	//determine overall polarity for the book based on all reviews		
+	    	System.out.println("The book is well worth a read :) !");
+	    else			
+	    	System.out.println("The book is not worth a read :("); 
+	    	
 	}
 
 
 	public static void SentiCode(String pathToSWN) throws IOException {
 		
-		// This is our main dictionary representation
-	
-
-		// From String to list of doubles.
+		// hashmap from String to list of integer double pairs
 		HashMap<String, HashMap<Integer, Double>> tempDictionary = new HashMap<String, HashMap<Integer, Double>>();
 
 		BufferedReader csv = null;
 		try {
-			csv = new BufferedReader(new FileReader(pathToSWN));
+			csv = new BufferedReader(new FileReader(pathToSWN)); //read from the txt file
 			int lineNumber = 0;
 
 			String line;
 			while ((line = csv.readLine()) != null) {
 				lineNumber++;
 
-				// If it's a comment, skip this line.
+				// id the line is a comment, skip this line
 				if (!line.trim().startsWith("#")) {
-					// We use tab separation
+					// use tab separation to split the line into array of strings
 					String[] data = line.split("\t");
-					String wordTypeMarker = data[0];
+					String wordTypeMarker = data[0]; //pos
 
-					// Example line:
-					// POS ID PosS NegS SynsetTerm#sensenumber Desc
-					// a 00009618 0.5 0.25 spartan#4 austere#3 ascetical#2
-					// ascetic#2 practicing great self-denial;...etc
-
-					// Is it a valid line? Otherwise, through exception.
+					// throw an exception for invalid lines
 					if (data.length != 6) {
-						throw new IllegalArgumentException(
-								"Incorrect tabulation format in file, line: "
-										+ lineNumber);
+						throw new IllegalArgumentException("Incorrect tabulation format in file, line: " + lineNumber);
 					}
 
-					// Calculate synset score as score = PosS - NegS
-					Double synsetScore = Double.parseDouble(data[2])
-							- Double.parseDouble(data[3]);
+					// calculate synset score as score = PosS - NegS
+					Double synsetScore = Double.parseDouble(data[2]) - Double.parseDouble(data[3]);
 
-					// Get all Synset terms
+					// get all synset terms in a line
 					String[] synTermsSplit = data[4].split(" ");
 
-					// Go through all terms of current synset.
+					// go through all terms of current synset.
 					for (String synTermSplit : synTermsSplit) {
-						// Get synterm and synterm rank
+						// get synterm and synterm rank
 						String[] synTermAndRank = synTermSplit.split("#");
-						String synTerm = synTermAndRank[0] + "#"
-								+ wordTypeMarker;
+						String synTerm = synTermAndRank[0] + "#" + wordTypeMarker;
 
 						int synTermRank = Integer.parseInt(synTermAndRank[1]);
-						// What we get here is a map of the type:
-						// term -> {score of synset#1, score of synset#2...}
 
-						// Add map to term if it doesn't have one
+						// add term to map if it doesn't have one
 						if (!tempDictionary.containsKey(synTerm)) {
-							tempDictionary.put(synTerm,
-									new HashMap<Integer, Double>());
+							tempDictionary.put(synTerm, new HashMap<Integer, Double>());
 						}
 
-						// Add synset link to synterm
-						tempDictionary.get(synTerm).put(synTermRank,
-								synsetScore);
+						// add synset link to synterm
+						tempDictionary.get(synTerm).put(synTermRank, synsetScore);
 					}
 				}
 			}
 
-			// Go through all the terms.
-			for (Map.Entry<String, HashMap<Integer, Double>> entry : tempDictionary
-					.entrySet()) {
+			// go through all the terms
+			for (Map.Entry<String, HashMap<Integer, Double>> entry : tempDictionary.entrySet()) {
 				String word = entry.getKey();
 				Map<Integer, Double> synSetScoreMap = entry.getValue();
 
-				// Calculate weighted average. Weigh the synsets according to
-				// their rank.
-				// Score= 1/2*first + 1/3*second + 1/4*third ..... etc.
-				// Sum = 1/1 + 1/2 + 1/3 ...
+				// calculate weighted average - weigh the synsets according to their rank
 				double score = 0.0;
 				double sum = 0.0;
-				for (Map.Entry<Integer, Double> setScore : synSetScoreMap
-						.entrySet()) {
+				for (Map.Entry<Integer, Double> setScore : synSetScoreMap.entrySet()) {
 					score += setScore.getValue() / (double) setScore.getKey();
 					sum += 1.0 / (double) setScore.getKey();
 				}
 				score /= sum;
 
-				dictionary.put(word, score);
+				dictionary.put(word, score); //populate final dictionary
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -205,11 +205,27 @@ public class HelloWorld {
 		}
 	}
 
+	//function to get score of word from dictionary
 	public static double extract(String word, String pos) {
 		if(dictionary.get(word + "#" + pos) != null)
 			return dictionary.get(word + "#" + pos);
 		else
 			return 0.0;
+	}
+	
+	//function to list all files in a given directory
+	public static void listf(String directoryName, ArrayList<File> files) {
+	    File directory = new File(directoryName);
+
+	    // get all the files from a directory
+	    File[] fList = directory.listFiles();
+	    for (File file : fList) {
+	        if (file.isFile()) {
+	            files.add(file);
+	        } else if (file.isDirectory()) {
+	            listf(file.getAbsolutePath(), files);
+	        }
+	    }
 	}
 	
 }
